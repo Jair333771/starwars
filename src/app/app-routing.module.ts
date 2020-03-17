e352f0450d1ec4c858c09f53b9f4be1ee00fcfa1
Jair ®odriguez <jair3337@hotmail.com>
2020-03-16 19:02:06 -0500

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Inicio' }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
