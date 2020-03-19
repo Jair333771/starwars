7b34798ad51281cb074e12b96f137a186c940475
Jair ®odriguez <jairodriguez3337@gmail.com>
2020-03-18 21:15:26 -0500

    add items on home elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public elemenstHome: any;
+  public root = "/assets/images/elements/"
 
-  ngOnInit(): void {
+  constructor() {    
   }
 
+  ngOnInit(): void {
+    this.getElement();
+  }
+
+  getElement(){
+    this.elemenstHome = [
+      {
+        id: 1,
+        properties: [
+          { name: 'Personajes', image: 'people.jpg', link: "#" },
+          { name: 'Planetas', image: 'planet.jpg', link: "#" },
+        ]
+      },
+      {
+        id: 2,
+        properties: [
+          { name: 'Especies', image: 'characters.jpg', link: "#" },   
+          { name: 'Naves', image: 'starships.jpg', link: "#" },
+        ]
+      },
+      {
+        id: 3,
+        properties: [
+          { name: 'Vehículos', image: 'vehicle.jpg', link: "#" },
+          { name: 'Films', image: 'films.jpg', link: "#" },
+        ]
+      },
+    ];
+  }
 }
