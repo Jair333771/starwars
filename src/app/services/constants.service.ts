6006ff3ddf6a492b612f318ebb54024663ebb972
miguelbermudezz <miguel.aki@hotmail.com>
2020-03-24 02:18:18 -0500

    Update constants.service.ts
    Cambio en el Menu de la palabra People por Characters

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ConstantsService {
 
   menu = [
-    { display: 'People', url: '#' },
+    { display: 'Characters', url: '#' },
     { display: 'Planets', url: '#' },
     { display: 'Films', url: '#' },
     { display: 'species', url: '#' },
