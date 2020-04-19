3593ab932d9230d47f1d97983f0fbc3804fb5745
Steven Rodriguez <stevenrodrod@gmail.com>
2020-04-18 19:30:54 -0500

    Inclusión de utilidades, clase para eliminar guion bajo de los textos mediante filtros angular

---
+++ src/app/utilities/remove-underscore-pipe.spec.ts
@@ -0,0 +1,7 @@
+import { RemoveUnderscorePipe } from './remove-underscore-pipe';
+
+describe('RemoveUnderscorePipe', () => {
+  it('should create an instance', () => {
+    expect(new RemoveUnderscorePipe()).toBeTruthy();
+  });
+});
